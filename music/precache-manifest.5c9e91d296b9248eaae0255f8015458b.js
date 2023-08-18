1b435f1d75d9efb909413b8c1887f21e8f455375
MAX208V <48180524+MAX208V@users.noreply.github.com>
2023-08-18 19:08:50 +0800

    Add files via upload

---
+++ music/precache-manifest.5c9e91d296b9248eaae0255f8015458b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c2b77e47bd914f2d07",
+    "url": "css/app.58add593.css"
+  },
+  {
+    "revision": "c904814d8db29c34bf50",
+    "url": "css/chunk-vendors.8cf7dd44.css"
+  },
+  {
+    "revision": "f326eafb67c0a0de5af3d2a263f5cd15",
+    "url": "fc920127cb0a23582f47.worker.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "35bf7b0b947939f833a8bc7fdbddf68d",
+    "url": "index.html"
+  },
+  {
+    "revision": "f0c2b77e47bd914f2d07",
+    "url": "js/app.51e9a04a.js"
+  },
+  {
+    "revision": "c904814d8db29c34bf50",
+    "url": "js/chunk-vendors.3918e9d0.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "static/kgm.mask"
+  }
+]);
