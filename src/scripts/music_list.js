28b6e6ee6fb27e0f83776e9c7414949c0d53e5ff
MaxCloud Dev <maxcloud@dev.local>
2026-06-29 14:15:18 +0000

    update music list (15 songs)

--- src/scripts/music_list.js
+++ src/scripts/music_list.js
@@ -1,198 +1,105 @@
 var musicList = [
   {
-    "title": "Santorini（圣托里尼）",
-    "artist": "Jasing Rye",
-    "src": "./public/music/Jasing%20Rye%20-%20Santorini%EF%BC%88%E5%9C%A3%E6%89%98%E9%87%8C%E5%B0%BC%EF%BC%89.mp3",
-    "cover": "./public/music/Jasing%20Rye%20-%20Santorini%EF%BC%88%E5%9C%A3%E6%89%98%E9%87%8C%E5%B0%BC%EF%BC%89.jpg",
-    "lrc": "./public/music/Jasing%20Rye%20-%20Santorini%EF%BC%88%E5%9C%A3%E6%89%98%E9%87%8C%E5%B0%BC%EF%BC%89.lrc"
+    "title": "空空",
+    "artist": "刘烨溦",
+    "src": "./public/music/%E5%88%98%E7%83%A8%E6%BA%A6%20-%20%E7%A9%BA%E7%A9%BA.mp3",
+    "cover": "./public/music/%E5%88%98%E7%83%A8%E6%BA%A6%20-%20%E7%A9%BA%E7%A9%BA.jpg",
+    "lrc": "./public/music/%E5%88%98%E7%83%A8%E6%BA%A6%20-%20%E7%A9%BA%E7%A9%BA.lrc"
   },
   {
-    "title": "Une Hirondelle A Fait Le Printemps（独燕成春）",
-    "artist": "Jasing Rye",
-    "src": "./public/music/Jasing%20Rye%20-%20Une%20Hirondelle%20A%20Fait%20Le%20Printemps%EF%BC%88%E7%8B%AC%E7%87%95%E6%88%90%E6%98%A5%EF%BC%89.mp3",
-    "cover": "./public/music/Jasing%20Rye%20-%20Une%20Hirondelle%20A%20Fait%20Le%20Printemps%EF%BC%88%E7%8B%AC%E7%87%95%E6%88%90%E6%98%A5%EF%BC%89.jpg",
-    "lrc": "./public/music/Jasing%20Rye%20-%20Une%20Hirondelle%20A%20Fait%20Le%20Printemps%EF%BC%88%E7%8B%AC%E7%87%95%E6%88%90%E6%98%A5%EF%BC%89.lrc"
+    "title": "阿房宫赋",
+    "artist": "小淅儿",
+    "src": "./public/music/%E5%B0%8F%E6%B7%85%E5%84%BF%20-%20%E9%98%BF%E6%88%BF%E5%AE%AB%E8%B5%8B.mp3",
+    "cover": "./public/music/%E5%B0%8F%E6%B7%85%E5%84%BF%20-%20%E9%98%BF%E6%88%BF%E5%AE%AB%E8%B5%8B.jpg",
+    "lrc": "./public/music/%E5%B0%8F%E6%B7%85%E5%84%BF%20-%20%E9%98%BF%E6%88%BF%E5%AE%AB%E8%B5%8B.lrc"
   },
   {
-    "title": "La Ultima Noche(Collazo)",
-    "artist": "Lex Vandyke",
-    "src": "./public/music/Lex%20Vandyke%20-%20La%20Ultima%20Noche(Collazo).mp3",
-    "cover": "./public/music/Lex%20Vandyke%20-%20La%20Ultima%20Noche(Collazo).jpg",
-    "lrc": "./public/music/Lex%20Vandyke%20-%20La%20Ultima%20Noche(Collazo).lrc"
+    "title": "Last Dance (Live版)",
+    "artist": "张信哲",
+    "src": "./public/music/%E5%BC%A0%E4%BF%A1%E5%93%B2%3B%20%E4%B8%87%E8%8A%B3%3B%20%E8%8E%AB%E6%96%87%E8%94%9A%3B%20%E6%9D%A8%E4%B9%83%E6%96%87%3B%20%E5%BC%A0%E6%9D%B0%3B%20%E5%BC%A0%E9%9F%B6%E6%B6%B5%3B%20%E6%9D%A8%E5%AE%97%E7%BA%AC%3B%20%E4%BA%8E%E6%96%87%E6%96%87%3B%20%E9%9F%A6%E7%A4%BC%E5%AE%89%3B%20%E6%AA%80%E5%81%A5%E6%AC%A1%3B%20%E5%85%A8%E4%BD%93%E8%A7%82%E4%BC%97%20-%20Last%20Dance%20(Live%E7%89%88).mp3",
+    "cover": "./public/music/%E5%BC%A0%E4%BF%A1%E5%93%B2%3B%20%E4%B8%87%E8%8A%B3%3B%20%E8%8E%AB%E6%96%87%E8%94%9A%3B%20%E6%9D%A8%E4%B9%83%E6%96%87%3B%20%E5%BC%A0%E6%9D%B0%3B%20%E5%BC%A0%E9%9F%B6%E6%B6%B5%3B%20%E6%9D%A8%E5%AE%97%E7%BA%AC%3B%20%E4%BA%8E%E6%96%87%E6%96%87%3B%20%E9%9F%A6%E7%A4%BC%E5%AE%89%3B%20%E6%AA%80%E5%81%A5%E6%AC%A1%3B%20%E5%85%A8%E4%BD%93%E8%A7%82%E4%BC%97%20-%20Last%20Dance%20(Live%E7%89%88).png",
+    "lrc": "./public/music/%E5%BC%A0%E4%BF%A1%E5%93%B2%3B%20%E4%B8%87%E8%8A%B3%3B%20%E8%8E%AB%E6%96%87%E8%94%9A%3B%20%E6%9D%A8%E4%B9%83%E6%96%87%3B%20%E5%BC%A0%E6%9D%B0%3B%20%E5%BC%A0%E9%9F%B6%E6%B6%B5%3B%20%E6%9D%A8%E5%AE%97%E7%BA%AC%3B%20%E4%BA%8E%E6%96%87%E6%96%87%3B%20%E9%9F%A6%E7%A4%BC%E5%AE%89%3B%20%E6%AA%80%E5%81%A5%E6%AC%A1%3B%20%E5%85%A8%E4%BD%93%E8%A7%82%E4%BC%97%20-%20Last%20Dance%20(Live%E7%89%88).lrc"
   },
   {
-    "title": "Follow YourDream's",
-    "artist": "MerOne Record's",
-    "src": "./public/music/MerOne%20Record's%20-%20Follow%20YourDream's.mp3",
-    "cover": "./public/music/MerOne%20Record's%20-%20Follow%20YourDream's.jpg",
-    "lrc": "./public/music/MerOne%20Record's%20-%20Follow%20YourDream's.lrc"
+    "title": "金梭和银梭",
+    "artist": "朱逢博",
+    "src": "./public/music/%E6%9C%B1%E9%80%A2%E5%8D%9A%20-%20%E9%87%91%E6%A2%AD%E5%92%8C%E9%93%B6%E6%A2%AD.mp3",
+    "cover": "./public/music/%E6%9C%B1%E9%80%A2%E5%8D%9A%20-%20%E9%87%91%E6%A2%AD%E5%92%8C%E9%93%B6%E6%A2%AD.jpg",
+    "lrc": "./public/music/%E6%9C%B1%E9%80%A2%E5%8D%9A%20-%20%E9%87%91%E6%A2%AD%E5%92%8C%E9%93%B6%E6%A2%AD.lrc"
   },
   {
-    "title": "【入梦】竹梦令",
-    "artist": "Tacke竹桑",
-    "src": "./public/music/Tacke%E7%AB%B9%E6%A1%91%20-%20%E3%80%90%E5%85%A5%E6%A2%A6%E3%80%91%E7%AB%B9%E6%A2%A6%E4%BB%A4.mp3",
-    "cover": "./public/music/Tacke%E7%AB%B9%E6%A1%91%20-%20%E3%80%90%E5%85%A5%E6%A2%A6%E3%80%91%E7%AB%B9%E6%A2%A6%E4%BB%A4.jpg",
-    "lrc": "./public/music/Tacke%E7%AB%B9%E6%A1%91%20-%20%E3%80%90%E5%85%A5%E6%A2%A6%E3%80%91%E7%AB%B9%E6%A2%A6%E4%BB%A4.lrc"
+    "title": "越过山丘",
+    "artist": "杨宗纬",
+    "src": "./public/music/%E6%9D%A8%E5%AE%97%E7%BA%AC%20-%20%E8%B6%8A%E8%BF%87%E5%B1%B1%E4%B8%98.mp3",
+    "cover": "./public/music/%E6%9D%A8%E5%AE%97%E7%BA%AC%20-%20%E8%B6%8A%E8%BF%87%E5%B1%B1%E4%B8%98.jpg",
+    "lrc": "./public/music/%E6%9D%A8%E5%AE%97%E7%BA%AC%20-%20%E8%B6%8A%E8%BF%87%E5%B1%B1%E4%B8%98.lrc"
   },
   {
-    "title": "溯梦.溯梦千年",
-    "artist": "Tacke竹桑",
-    "src": "./public/music/Tacke%E7%AB%B9%E6%A1%91%20-%20%E6%BA%AF%E6%A2%A6.%E6%BA%AF%E6%A2%A6%E5%8D%83%E5%B9%B4.mp3",
-    "cover": "./public/music/Tacke%E7%AB%B9%E6%A1%91%20-%20%E6%BA%AF%E6%A2%A6.%E6%BA%AF%E6%A2%A6%E5%8D%83%E5%B9%B4.jpg",
-    "lrc": "./public/music/Tacke%E7%AB%B9%E6%A1%91%20-%20%E6%BA%AF%E6%A2%A6.%E6%BA%AF%E6%A2%A6%E5%8D%83%E5%B9%B4.lrc"
+    "title": "日落大道 (Live)",
+    "artist": "梁博",
+    "src": "./public/music/%E6%A2%81%E5%8D%9A%20-%20%E6%97%A5%E8%90%BD%E5%A4%A7%E9%81%93%20(Live).mp3",
+    "cover": "./public/music/%E6%A2%81%E5%8D%9A%20-%20%E6%97%A5%E8%90%BD%E5%A4%A7%E9%81%93%20(Live).png",
+    "lrc": "./public/music/%E6%A2%81%E5%8D%9A%20-%20%E6%97%A5%E8%90%BD%E5%A4%A7%E9%81%93%20(Live).lrc"
   },
   {
-    "title": "香花",
-    "artist": "丘沛宸",
-    "src": "./public/music/%E4%B8%98%E6%B2%9B%E5%AE%B8%20-%20%E9%A6%99%E8%8A%B1.mp3",
-    "cover": "./public/music/%E4%B8%98%E6%B2%9B%E5%AE%B8%20-%20%E9%A6%99%E8%8A%B1.jpg",
-    "lrc": "./public/music/%E4%B8%98%E6%B2%9B%E5%AE%B8%20-%20%E9%A6%99%E8%8A%B1.lrc"
+    "title": "阳光男孩阳光女孩2023",
+    "artist": "法老; 小精灵; 万妮达Vinida Weng; 喉结蜥蜴",
+    "src": "./public/music/%E6%B3%95%E8%80%81%3B%20%E5%B0%8F%E7%B2%BE%E7%81%B5%3B%20%E4%B8%87%E5%A6%AE%E8%BE%BEVinida%20Weng%3B%20%E5%96%89%E7%BB%93%E8%9C%A5%E8%9C%B4%20-%20%E9%98%B3%E5%85%89%E7%94%B7%E5%AD%A9%E9%98%B3%E5%85%89%E5%A5%B3%E5%AD%A92023.mp3"
   },
   {
-    "title": "忧伤湖泊",
-    "artist": "傀儡乐队PUPPET 老狼",
-    "src": "./public/music/%E5%82%80%E5%84%A1%E4%B9%90%E9%98%9FPUPPET%20%E8%80%81%E7%8B%BC%20-%20%E5%BF%A7%E4%BC%A4%E6%B9%96%E6%B3%8A.mp3",
-    "cover": "./public/music/%E5%82%80%E5%84%A1%E4%B9%90%E9%98%9FPUPPET%20%E8%80%81%E7%8B%BC%20-%20%E5%BF%A7%E4%BC%A4%E6%B9%96%E6%B3%8A.jpg",
-    "lrc": "./public/music/%E5%82%80%E5%84%A1%E4%B9%90%E9%98%9FPUPPET%20%E8%80%81%E7%8B%BC%20-%20%E5%BF%A7%E4%BC%A4%E6%B9%96%E6%B3%8A.lrc"
+    "title": "靠近",
+    "artist": "罗震环",
+    "src": "./public/music/%E7%BD%97%E9%9C%87%E7%8E%AF%20-%20%E9%9D%A0%E8%BF%91.mp3",
+    "cover": "./public/music/%E7%BD%97%E9%9C%87%E7%8E%AF%20-%20%E9%9D%A0%E8%BF%91.jpg",
+    "lrc": "./public/music/%E7%BD%97%E9%9C%87%E7%8E%AF%20-%20%E9%9D%A0%E8%BF%91.lrc"
   },
   {
-    "title": "涯兜",
-    "artist": "刘莉汕",
-    "src": "./public/music/%E5%88%98%E8%8E%89%E6%B1%95%20-%20%E6%B6%AF%E5%85%9C.mp3",
-    "cover": "./public/music/%E5%88%98%E8%8E%89%E6%B1%95%20-%20%E6%B6%AF%E5%85%9C.jpg",
-    "lrc": "./public/music/%E5%88%98%E8%8E%89%E6%B1%95%20-%20%E6%B6%AF%E5%85%9C.lrc"
+    "title": "岁月里的花（live）",
+    "artist": "莫文蔚",
+    "src": "./public/music/%E8%8E%AB%E6%96%87%E8%94%9A%20%E6%AF%9B%E4%B8%8D%E6%98%93%20-%20%E6%AD%8C%E6%9B%B2%EF%BC%9A%E5%B2%81%E6%9C%88%E9%87%8C%E7%9A%84%E8%8A%B1%EF%BC%88live%EF%BC%89.mp3",
+    "cover": "./public/music/%E8%8E%AB%E6%96%87%E8%94%9A%20%E6%AF%9B%E4%B8%8D%E6%98%93%20-%20%E6%AD%8C%E6%9B%B2%EF%BC%9A%E5%B2%81%E6%9C%88%E9%87%8C%E7%9A%84%E8%8A%B1%EF%BC%88live%EF%BC%89.jpg",
+    "lrc": "./public/music/%E8%8E%AB%E6%96%87%E8%94%9A%20%E6%AF%9B%E4%B8%8D%E6%98%93%20-%20%E6%AD%8C%E6%9B%B2%EF%BC%9A%E5%B2%81%E6%9C%88%E9%87%8C%E7%9A%84%E8%8A%B1%EF%BC%88live%EF%BC%89.lrc"
   },
   {
-    "title": "凶手不只一个",
-    "artist": "小人",
-    "src": "./public/music/%E5%B0%8F%E4%BA%BA%20-%20%E5%87%B6%E6%89%8B%E4%B8%8D%E5%8F%AA%E4%B8%80%E4%B8%AA.mp3",
-    "cover": "./public/music/%E5%B0%8F%E4%BA%BA%20-%20%E5%87%B6%E6%89%8B%E4%B8%8D%E5%8F%AA%E4%B8%80%E4%B8%AA.jpg",
-    "lrc": "./public/music/%E5%B0%8F%E4%BA%BA%20-%20%E5%87%B6%E6%89%8B%E4%B8%8D%E5%8F%AA%E4%B8%80%E4%B8%AA.lrc"
+    "title": "远走高飞",
+    "artist": "金志文",
+    "src": "./public/music/%E9%87%91%E5%BF%97%E6%96%87%20-%20%E8%BF%9C%E8%B5%B0%E9%AB%98%E9%A3%9E.mp3",
+    "cover": "./public/music/%E9%87%91%E5%BF%97%E6%96%87%20-%20%E8%BF%9C%E8%B5%B0%E9%AB%98%E9%A3%9E.jpg",
+    "lrc": "./public/music/%E9%87%91%E5%BF%97%E6%96%87%20-%20%E8%BF%9C%E8%B5%B0%E9%AB%98%E9%A3%9E.lrc"
   },
   {
-    "title": "剑伤",
-    "artist": "李易峰",
-    "src": "./public/music/%E6%9D%8E%E6%98%93%E5%B3%B0%20-%20%E5%89%91%E4%BC%A4.mp3",
-    "cover": "./public/music/%E6%9D%8E%E6%98%93%E5%B3%B0%20-%20%E5%89%91%E4%BC%A4.jpg",
-    "lrc": "./public/music/%E6%9D%8E%E6%98%93%E5%B3%B0%20-%20%E5%89%91%E4%BC%A4.lrc"
+    "title": "青城山下白素贞",
+    "artist": "银临",
+    "src": "./public/music/%E9%93%B6%E4%B8%B4%20-%20%E9%9D%92%E5%9F%8E%E5%B1%B1%E4%B8%8B%E7%99%BD%E7%B4%A0%E8%B4%9E.mp3",
+    "cover": "./public/music/%E9%93%B6%E4%B8%B4%20-%20%E9%9D%92%E5%9F%8E%E5%B1%B1%E4%B8%8B%E7%99%BD%E7%B4%A0%E8%B4%9E.png",
+    "lrc": "./public/music/%E9%93%B6%E4%B8%B4%20-%20%E9%9D%92%E5%9F%8E%E5%B1%B1%E4%B8%8B%E7%99%BD%E7%B4%A0%E8%B4%9E.lrc"
   },
   {
-    "title": "北海",
-    "artist": "永动机",
-    "src": "./public/music/%E6%B0%B8%E5%8A%A8%E6%9C%BA%20-%20%E5%8C%97%E6%B5%B7.mp3",
-    "cover": "./public/music/%E6%B0%B8%E5%8A%A8%E6%9C%BA%20-%20%E5%8C%97%E6%B5%B7.jpg",
-    "lrc": "./public/music/%E6%B0%B8%E5%8A%A8%E6%9C%BA%20-%20%E5%8C%97%E6%B5%B7.lrc"
+    "title": "青玉案·元夕 (Live)",
+    "artist": "陈彼得",
+    "src": "./public/music/%E9%99%88%E5%BD%BC%E5%BE%97%20-%20%E9%9D%92%E7%8E%89%E6%A1%88%E3%83%BB%E5%85%83%E5%A4%95%20(Live).MP3",
+    "cover": "./public/music/%E9%99%88%E5%BD%BC%E5%BE%97%20-%20%E9%9D%92%E7%8E%89%E6%A1%88%E3%83%BB%E5%85%83%E5%A4%95%20(Live).MP3.jpg",
+    "lrc": "./public/music/%E9%99%88%E5%BD%BC%E5%BE%97%20-%20%E9%9D%92%E7%8E%89%E6%A1%88%E3%83%BB%E5%85%83%E5%A4%95%20(Live).MP3.lrc"
   },
   {
-    "title": "圆桌骑士",
-    "artist": "汤颖",
-    "src": "./public/music/%E6%B1%A4%E9%A2%96%20-%20%E5%9C%86%E6%A1%8C%E9%AA%91%E5%A3%AB.mp3",
-    "cover": "./public/music/%E6%B1%A4%E9%A2%96%20-%20%E5%9C%86%E6%A1%8C%E9%AA%91%E5%A3%AB.jpg",
-    "lrc": "./public/music/%E6%B1%A4%E9%A2%96%20-%20%E5%9C%86%E6%A1%8C%E9%AA%91%E5%A3%AB.lrc"
-  },
-  {
-    "title": "请到天涯海角来",
-    "artist": "沈小岑",
-    "src": "./public/music/%E6%B2%88%E5%B0%8F%E5%B2%91%20-%20%E8%AF%B7%E5%88%B0%E5%A4%A9%E6%B6%AF%E6%B5%B7%E8%A7%92%E6%9D%A5.mp3",
-    "cover": "./public/music/%E6%B2%88%E5%B0%8F%E5%B2%91%20-%20%E8%AF%B7%E5%88%B0%E5%A4%A9%E6%B6%AF%E6%B5%B7%E8%A7%92%E6%9D%A5.jpg",
-    "lrc": "./public/music/%E6%B2%88%E5%B0%8F%E5%B2%91%20-%20%E8%AF%B7%E5%88%B0%E5%A4%A9%E6%B6%AF%E6%B5%B7%E8%A7%92%E6%9D%A5.lrc"
-  },
-  {
-    "title": "金山寺",
-    "artist": "燕池",
-    "src": "./public/music/%E7%87%95%E6%B1%A0%20-%20%E9%87%91%E5%B1%B1%E5%AF%BA.mp3",
-    "cover": "./public/music/%E7%87%95%E6%B1%A0%20-%20%E9%87%91%E5%B1%B1%E5%AF%BA.jpg",
-    "lrc": "./public/music/%E7%87%95%E6%B1%A0%20-%20%E9%87%91%E5%B1%B1%E5%AF%BA.lrc"
-  },
-  {
-    "title": "坐忘道",
-    "artist": "王朝1982",
-    "src": "./public/music/%E7%8E%8B%E6%9C%9D1982%20-%20%E5%9D%90%E5%BF%98%E9%81%93.mp3",
-    "cover": "./public/music/%E7%8E%8B%E6%9C%9D1982%20-%20%E5%9D%90%E5%BF%98%E9%81%93.jpg",
-    "lrc": "./public/music/%E7%8E%8B%E6%9C%9D1982%20-%20%E5%9D%90%E5%BF%98%E9%81%93.lrc"
-  },
-  {
-    "title": "我本将心向明月",
-    "artist": "王朝1982 朱旭BooBoo",
-    "src": "./public/music/%E7%8E%8B%E6%9C%9D1982%20%E6%9C%B1%E6%97%ADBooBoo%20-%20%E6%88%91%E6%9C%AC%E5%B0%86%E5%BF%83%E5%90%91%E6%98%8E%E6%9C%88.mp3",
-    "cover": "./public/music/%E7%8E%8B%E6%9C%9D1982%20%E6%9C%B1%E6%97%ADBooBoo%20-%20%E6%88%91%E6%9C%AC%E5%B0%86%E5%BF%83%E5%90%91%E6%98%8E%E6%9C%88.jpg",
-    "lrc": "./public/music/%E7%8E%8B%E6%9C%9D1982%20%E6%9C%B1%E6%97%ADBooBoo%20-%20%E6%88%91%E6%9C%AC%E5%B0%86%E5%BF%83%E5%90%91%E6%98%8E%E6%9C%88.lrc"
-  },
-  {
-    "title": "择日飞仙",
-    "artist": "王朝1982 朱旭BooBoo",
-    "src": "./public/music/%E7%8E%8B%E6%9C%9D1982%20%E6%9C%B1%E6%97%ADBooBoo%20-%20%E6%8B%A9%E6%97%A5%E9%A3%9E%E4%BB%99.mp3",
-    "cover": "./public/music/%E7%8E%8B%E6%9C%9D1982%20%E6%9C%B1%E6%97%ADBooBoo%20-%20%E6%8B%A9%E6%97%A5%E9%A3%9E%E4%BB%99.jpg",
-    "lrc": "./public/music/%E7%8E%8B%E6%9C%9D1982%20%E6%9C%B1%E6%97%ADBooBoo%20-%20%E6%8B%A9%E6%97%A5%E9%A3%9E%E4%BB%99.lrc"
-  },
-  {
-    "title": "蓮花空行",
-    "artist": "珂拉琪 Collage",
-    "src": "./public/music/%E7%8F%82%E6%8B%89%E7%90%AA%20Collage%20-%20%E8%93%AE%E8%8A%B1%E7%A9%BA%E8%A1%8C.mp3",
-    "cover": "./public/music/%E7%8F%82%E6%8B%89%E7%90%AA%20Collage%20-%20%E8%93%AE%E8%8A%B1%E7%A9%BA%E8%A1%8C.jpg",
-    "lrc": "./public/music/%E7%8F%82%E6%8B%89%E7%90%AA%20Collage%20-%20%E8%93%AE%E8%8A%B1%E7%A9%BA%E8%A1%8C.lrc"
-  },
-  {
-    "title": "小时候（2022版）",
-    "artist": "罗伟力",
-    "src": "./public/music/%E7%BD%97%E4%BC%9F%E5%8A%9B%20-%20%E5%B0%8F%E6%97%B6%E5%80%99%EF%BC%882022%E7%89%88%EF%BC%89.mp3",
-    "cover": "./public/music/%E7%BD%97%E4%BC%9F%E5%8A%9B%20-%20%E5%B0%8F%E6%97%B6%E5%80%99%EF%BC%882022%E7%89%88%EF%BC%89.jpg",
-    "lrc": "./public/music/%E7%BD%97%E4%BC%9F%E5%8A%9B%20-%20%E5%B0%8F%E6%97%B6%E5%80%99%EF%BC%882022%E7%89%88%EF%BC%89.lrc"
-  },
-  {
-    "title": "车田豆腐（2022版）",
-    "artist": "罗伟力",
-    "src": "./public/music/%E7%BD%97%E4%BC%9F%E5%8A%9B%20-%20%E8%BD%A6%E7%94%B0%E8%B1%86%E8%85%90%EF%BC%882022%E7%89%88%EF%BC%89.mp3",
-    "cover": "./public/music/%E7%BD%97%E4%BC%9F%E5%8A%9B%20-%20%E8%BD%A6%E7%94%B0%E8%B1%86%E8%85%90%EF%BC%882022%E7%89%88%EF%BC%89.jpg",
-    "lrc": "./public/music/%E7%BD%97%E4%BC%9F%E5%8A%9B%20-%20%E8%BD%A6%E7%94%B0%E8%B1%86%E8%85%90%EF%BC%882022%E7%89%88%EF%BC%89.lrc"
-  },
-  {
-    "title": "之乎者也",
-    "artist": "罗大佑",
-    "src": "./public/music/%E7%BD%97%E5%A4%A7%E4%BD%91%20-%20%E4%B9%8B%E4%B9%8E%E8%80%85%E4%B9%9F.mp3",
-    "cover": "./public/music/%E7%BD%97%E5%A4%A7%E4%BD%91%20-%20%E4%B9%8B%E4%B9%8E%E8%80%85%E4%B9%9F.png",
-    "lrc": "./public/music/%E7%BD%97%E5%A4%A7%E4%BD%91%20-%20%E4%B9%8B%E4%B9%8E%E8%80%85%E4%B9%9F.lrc"
-  },
-  {
-    "title": "乡愁四韵",
-    "artist": "罗大佑",
-    "src": "./public/music/%E7%BD%97%E5%A4%A7%E4%BD%91%20-%20%E4%B9%A1%E6%84%81%E5%9B%9B%E9%9F%B5.mp3",
-    "cover": "./public/music/%E7%BD%97%E5%A4%A7%E4%BD%91%20-%20%E4%B9%A1%E6%84%81%E5%9B%9B%E9%9F%B5.png",
-    "lrc": "./public/music/%E7%BD%97%E5%A4%A7%E4%BD%91%20-%20%E4%B9%A1%E6%84%81%E5%9B%9B%E9%9F%B5.lrc"
-  },
-  {
-    "title": "军港之夜",
-    "artist": "苏小明",
-    "src": "./public/music/%E8%8B%8F%E5%B0%8F%E6%98%8E%20-%20%E5%86%9B%E6%B8%AF%E4%B9%8B%E5%A4%9C.mp3",
-    "cover": "./public/music/%E8%8B%8F%E5%B0%8F%E6%98%8E%20-%20%E5%86%9B%E6%B8%AF%E4%B9%8B%E5%A4%9C.jpg",
-    "lrc": "./public/music/%E8%8B%8F%E5%B0%8F%E6%98%8E%20-%20%E5%86%9B%E6%B8%AF%E4%B9%8B%E5%A4%9C.lrc"
-  },
-  {
-    "title": "万事镭行先",
-    "artist": "邱清云",
-    "src": "./public/music/%E9%82%B1%E6%B8%85%E4%BA%91%20-%20%E4%B8%87%E4%BA%8B%E9%95%AD%E8%A1%8C%E5%85%88.mp3",
-    "cover": "./public/music/%E9%82%B1%E6%B8%85%E4%BA%91%20-%20%E4%B8%87%E4%BA%8B%E9%95%AD%E8%A1%8C%E5%85%88.jpg",
-    "lrc": "./public/music/%E9%82%B1%E6%B8%85%E4%BA%91%20-%20%E4%B8%87%E4%BA%8B%E9%95%AD%E8%A1%8C%E5%85%88.lrc"
-  },
-  {
-    "title": "冇钱索货屎",
-    "artist": "邱清云",
-    "src": "./public/music/%E9%82%B1%E6%B8%85%E4%BA%91%20-%20%E5%86%87%E9%92%B1%E7%B4%A2%E8%B4%A7%E5%B1%8E.mp3",
-    "cover": "./public/music/%E9%82%B1%E6%B8%85%E4%BA%91%20-%20%E5%86%87%E9%92%B1%E7%B4%A2%E8%B4%A7%E5%B1%8E.jpg",
-    "lrc": "./public/music/%E9%82%B1%E6%B8%85%E4%BA%91%20-%20%E5%86%87%E9%92%B1%E7%B4%A2%E8%B4%A7%E5%B1%8E.lrc"
-  },
-  {
-    "title": "摘（Leaves fall, dreams rise）",
+    "title": "踮起脚就能够得到自由",
     "artist": "陈抒妮",
-    "src": "./public/music/%E9%99%88%E6%8A%92%E5%A6%AE%20-%20%E6%91%98%EF%BC%88Leaves%20fall%2C%20dreams%20rise%EF%BC%89.mp3",
-    "cover": "./public/music/%E9%99%88%E6%8A%92%E5%A6%AE%20-%20%E6%91%98%EF%BC%88Leaves%20fall%2C%20dreams%20rise%EF%BC%89.jpg",
-    "lrc": "./public/music/%E9%99%88%E6%8A%92%E5%A6%AE%20-%20%E6%91%98%EF%BC%88Leaves%20fall%2C%20dreams%20rise%EF%BC%89.lrc"
+    "src": "./public/music/%E9%99%88%E6%8A%92%E5%A6%AE%20-%20%E8%B8%AE%E8%B5%B7%E8%84%9A%E5%B0%B1%E8%83%BD%E5%A4%9F%E5%BE%97%E5%88%B0%E8%87%AA%E7%94%B1.mp3",
+    "cover": "./public/music/%E9%99%88%E6%8A%92%E5%A6%AE%20-%20%E8%B8%AE%E8%B5%B7%E8%84%9A%E5%B0%B1%E8%83%BD%E5%A4%9F%E5%BE%97%E5%88%B0%E8%87%AA%E7%94%B1.jpg",
+    "lrc": "./public/music/%E9%99%88%E6%8A%92%E5%A6%AE%20-%20%E8%B8%AE%E8%B5%B7%E8%84%9A%E5%B0%B1%E8%83%BD%E5%A4%9F%E5%BE%97%E5%88%B0%E8%87%AA%E7%94%B1.lrc"
   },
   {
-    "title": "寂静的天空",
-    "artist": "黛青塔娜 HAYA乐团",
-    "src": "./public/music/%E9%BB%9B%E9%9D%92%E5%A1%94%E5%A8%9C%20HAYA%E4%B9%90%E5%9B%A2%20-%20%E5%AF%82%E9%9D%99%E7%9A%84%E5%A4%A9%E7%A9%BA.mp3",
-    "cover": "./public/music/%E9%BB%9B%E9%9D%92%E5%A1%94%E5%A8%9C%20HAYA%E4%B9%90%E5%9B%A2%20-%20%E5%AF%82%E9%9D%99%E7%9A%84%E5%A4%A9%E7%A9%BA.jpg",
-    "lrc": "./public/music/%E9%BB%9B%E9%9D%92%E5%A1%94%E5%A8%9C%20HAYA%E4%B9%90%E5%9B%A2%20-%20%E5%AF%82%E9%9D%99%E7%9A%84%E5%A4%A9%E7%A9%BA.lrc"
+    "title": "走四方",
+    "artist": "韩磊",
+    "src": "./public/music/%E9%9F%A9%E7%A3%8A%20-%20%E8%B5%B0%E5%9B%9B%E6%96%B9.mp3",
+    "cover": "./public/music/%E9%9F%A9%E7%A3%8A%20-%20%E8%B5%B0%E5%9B%9B%E6%96%B9.jpg",
+    "lrc": "./public/music/%E9%9F%A9%E7%A3%8A%20-%20%E8%B5%B0%E5%9B%9B%E6%96%B9.lrc"
+  },
+  {
+    "title": "雪见—仙凡之旅",
+    "artist": "麦振鸿",
+    "src": "./public/music/%E9%BA%A6%E6%8C%AF%E9%B8%BF%20-%20%E9%9B%AA%E8%A7%81%E2%80%94%E4%BB%99%E5%87%A1%E4%B9%8B%E6%97%85.mp3",
+    "cover": "./public/music/%E9%BA%A6%E6%8C%AF%E9%B8%BF%20-%20%E9%9B%AA%E8%A7%81%E2%80%94%E4%BB%99%E5%87%A1%E4%B9%8B%E6%97%85.jpg",
+    "lrc": "./public/music/%E9%BA%A6%E6%8C%AF%E9%B8%BF%20-%20%E9%9B%AA%E8%A7%81%E2%80%94%E4%BB%99%E5%87%A1%E4%B9%8B%E6%97%85.lrc"
   }
 ];
